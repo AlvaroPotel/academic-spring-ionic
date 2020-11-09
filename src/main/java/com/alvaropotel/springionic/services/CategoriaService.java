74880f81ddd1c565eb496f5a4c9523cda0051ff8
AlvaroPotel <alvaropotel@gmail.com>
2020-11-09 21:39:22 +0000

    Paginação com parametros opcionais na requisição

--- src/main/java/com/alvaropotel/springionic/services/CategoriaService.java
+++ src/main/java/com/alvaropotel/springionic/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.alvaropotel.springionic.domain.Categoria;
@@ -41,12 +44,16 @@ public class CategoriaService {
 			repo.deleteById(id);
 		}catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivem excluir uma categoria que possui produtos");
-			
 		}
 	}
 	
 	public List<Categoria> findAll(){
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 	
 	
