c00aba09311e1e94ffb75c686d88a703453674e8
AlvaroPotel <alvaropotel@gmail.com>
2020-11-21 17:22:53 +0000

    Busca de Produtos por nome e categorias

---
+++ src/main/java/com/alvaropotel/springionic/services/ProdutoService.java
@@ -0,0 +1,39 @@
+package com.alvaropotel.springionic.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.alvaropotel.springionic.domain.Categoria;
+import com.alvaropotel.springionic.domain.Produto;
+import com.alvaropotel.springionic.repositories.CategoriaRepository;
+import com.alvaropotel.springionic.repositories.ProdutoRepository;
+import com.alvaropotel.springionic.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repo;
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);	
+	}
+
+}
