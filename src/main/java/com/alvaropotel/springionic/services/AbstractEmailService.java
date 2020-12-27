2cd124cf4d74756e18bd98b9db6251cd45b91c3f
AlvaroPotel <alvaropotel@gmail.com>
2020-12-27 15:43:29 +0000

    Recuperar senha

--- src/main/java/com/alvaropotel/springionic/services/AbstractEmailService.java
+++ src/main/java/com/alvaropotel/springionic/services/AbstractEmailService.java
@@ -12,6 +12,7 @@ import org.thymeleaf.context.Context;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 
+import com.alvaropotel.springionic.domain.Cliente;
 import com.alvaropotel.springionic.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
@@ -67,6 +68,22 @@ public abstract class AbstractEmailService implements EmailService {
 		mmh.setSentDate(new Date(System.currentTimeMillis()));
 		mmh.setText(htmlFromTemplatePedido(obj), true);
 		return mimeMessage;
+	}
+	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
+
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: " + newPass);
+		return sm;
 	}
 
 }
