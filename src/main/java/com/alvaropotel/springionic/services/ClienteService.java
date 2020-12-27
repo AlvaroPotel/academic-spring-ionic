167fee5ae4b25c945894f43c84e19cbd92b378b4
AlvaroPotel <alvaropotel@gmail.com>
2020-12-27 12:04:22 +0000

    Restricao de conteudo: cliente so recupera ele mesmo

--- src/main/java/com/alvaropotel/springionic/services/ClienteService.java
+++ src/main/java/com/alvaropotel/springionic/services/ClienteService.java
@@ -15,11 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 import com.alvaropotel.springionic.domain.Cidade;
 import com.alvaropotel.springionic.domain.Cliente;
 import com.alvaropotel.springionic.domain.Endereco;
+import com.alvaropotel.springionic.domain.enums.Perfil;
 import com.alvaropotel.springionic.domain.enums.TipoCliente;
 import com.alvaropotel.springionic.dto.ClienteDTO;
 import com.alvaropotel.springionic.dto.ClienteNewDTO;
 import com.alvaropotel.springionic.repositories.ClienteRepository;
 import com.alvaropotel.springionic.repositories.EnderecoRepository;
+import com.alvaropotel.springionic.security.UserSS;
+import com.alvaropotel.springionic.services.exceptions.AuthorizationException;
 import com.alvaropotel.springionic.services.exceptions.DataIntegrityException;
 import com.alvaropotel.springionic.services.exceptions.ObjectNotFoundException;
 
@@ -36,6 +39,11 @@ public class ClienteService {
 	private BCryptPasswordEncoder pe;
 	
 	public Cliente find(Integer id) {
+		UserSS user = UserService.authenticated();
+		if (user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
---
+++ src/main/java/com/alvaropotel/springionic/services/UserService.java
@@ -0,0 +1,18 @@
+package com.alvaropotel.springionic.services;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.alvaropotel.springionic.security.UserSS;
+
+
+public class UserService {
+
+	public static UserSS authenticated() {
+		try {
+			return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		}
+		catch (Exception e) {
+			return null;
+		}
+	}
+}
