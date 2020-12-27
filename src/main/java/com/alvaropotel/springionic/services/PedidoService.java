57626f69e6d9b999d146a47abb2c4e9fdce35a38
AlvaroPotel <alvaropotel@gmail.com>
2020-12-27 12:21:51 +0000

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/alvaropotel/springionic/services/PedidoService.java
+++ src/main/java/com/alvaropotel/springionic/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.alvaropotel.springionic.domain.Cliente;
 import com.alvaropotel.springionic.domain.ItemPedido;
 import com.alvaropotel.springionic.domain.PagamentoComBoleto;
 import com.alvaropotel.springionic.domain.Pedido;
@@ -13,6 +17,8 @@ import com.alvaropotel.springionic.domain.enums.EstadoPagamento;
 import com.alvaropotel.springionic.repositories.ItemPedidoRepository;
 import com.alvaropotel.springionic.repositories.PagamentoRepository;
 import com.alvaropotel.springionic.repositories.PedidoRepository;
+import com.alvaropotel.springionic.security.UserSS;
+import com.alvaropotel.springionic.services.exceptions.AuthorizationException;
 import com.alvaropotel.springionic.services.exceptions.ObjectNotFoundException;
 
 
@@ -68,6 +74,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 	
 }
