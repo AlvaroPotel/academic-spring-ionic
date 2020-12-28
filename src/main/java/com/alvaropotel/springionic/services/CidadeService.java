877877983c608515a631ed415f852172ca99b12b
AlvaroPotel <alvaropotel@gmail.com>
2020-12-28 14:25:55 +0000

    Endpoints para buscar estados e cidades

---
+++ src/main/java/com/alvaropotel/springionic/services/CidadeService.java
@@ -0,0 +1,21 @@
+package com.alvaropotel.springionic.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alvaropotel.springionic.domain.Cidade;
+import com.alvaropotel.springionic.repositories.CidadeRepository;
+
+
+@Service
+public class CidadeService {
+
+	@Autowired
+	private CidadeRepository repo;
+
+	public List<Cidade> findByEstado(Integer estadoId) {
+		return repo.findCidades(estadoId);
+	}
+}
